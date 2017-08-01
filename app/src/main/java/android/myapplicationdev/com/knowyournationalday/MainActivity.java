8be2ea010f22cb183bea581d552f695142503a66
15017541 <15017541@myrp.edu.sg>
2017-08-01 12:42:04 +0800

    login dialog

--- app/src/main/java/android/myapplicationdev/com/knowyournationalday/MainActivity.java
+++ app/src/main/java/android/myapplicationdev/com/knowyournationalday/MainActivity.java
@@ -1,10 +1,23 @@
 package android.myapplicationdev.com.knowyournationalday;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +29,109 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        callLoginDialog();
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 R.layout.listview, array);
 
         ListView listView = (ListView) findViewById(R.id.lv);
         listView.setAdapter(adapter);
+    }
+
+    private void callLoginDialog(){
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout login =
+                (LinearLayout) inflater.inflate(R.layout.login, null);
+        final EditText etLogin = (EditText) login
+                .findViewById(R.id.editTextLogin);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Please Login")
+                .setView(login)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        if(etLogin.getText().toString() == "738964"){
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Invalid Access Code", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton("No Access Code", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Tally against the respective action item clicked
+        //  and implement the appropriate action
+        if (item.getItemId() == R.id.action_quit) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Quit?")
+                    // Set text for the positive button and the corresponding
+                    //  OnClickListener when it is clicked
+                    .setPositiveButton("Quit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    })
+                    // Set text for the negative button and the corresponding
+                    //  OnClickListener when it is clicked
+                    .setNegativeButton("Not Really", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+
+        } else if (item.getItemId() == R.id.action_send) {
+            String [] list = new String[] { "Email", "SMS"};
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Select the way to enrich your friend")
+                    // Set the list of items easily by just supplying an
+                    //  array of the items
+                    .setItems(list, new DialogInterface.OnClickListener() {
+                        // The parameter "which" is the item index
+                        // clicked, starting from 0
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (which == 0) {
+                                Intent email = new Intent(Intent.ACTION_SEND);
+                                // Put essentials like email address, subject & body text
+                                email.putExtra(Intent.EXTRA_EMAIL,
+                                        new String[]{"test@rp.edu.sg"});
+                                email.putExtra(Intent.EXTRA_SUBJECT,
+                                        "Test Email from C347");
+                                email.putExtra(Intent.EXTRA_TEXT,
+                                        Arrays.toString(array));
+
+                                // This MIME type indicates email
+                                email.setType("message/rfc822");
+
+                                // createChooser shows user a list of app that can handle
+                                // this MIME type, which is, email
+                                startActivity(Intent.createChooser(email,
+                                        "Choose an Email client :"));
+                            } else {
+                                SmsManager smsManager = SmsManager.getDefault();
+                                smsManager.sendTextMessage("12345678", null,  Arrays.toString(array), null, null);
+                            }
+                        }
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
